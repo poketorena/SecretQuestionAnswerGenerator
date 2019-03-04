913d337258c40513e7dc773afb8289b828f3f62d
poketorena <poketorena@users.noreply.github.com>
2019-03-05 02:14:33 +0900

    厳密でない場合はこれでヨシ！(๑•̀ㅂ•́)و✧

--- SecretQuestionAnswerGenerator/Program.cs
+++ SecretQuestionAnswerGenerator/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
 
-namespace SecretQuestionAnswerGenerator
+namespace RandomStringGeneratorFromKatakanaAndHiragana
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ひらがなのストリームを作る
+
+            var hiraganas = Observable.Generate(
+                    'あ',
+                    hiragana => hiragana <= 'ん',
+                    hiragana => ++hiragana,
+                    hiragana => hiragana)
+                    .ToEnumerable();
+
+            // カタカナのストリームを作る
+
+            var katakanas = Observable.Generate(
+                    'ア',
+                    katakana => katakana <= 'ン',
+                    katakana => ++katakana,
+                    katakana => katakana)
+                    .ToEnumerable();
+
+            // 合体
+
+            var concatedCollection = hiraganas
+                .Concat(katakanas);
+
+            // コレクションからランダムに20文字取り出す
+
+            foreach (var _ in Enumerable.Range(0, 20))
+            {
+                var fetchedCaracter = concatedCollection
+                    .ElementAt(new Random().Next(concatedCollection.Count()));
+
+                Console.Write(fetchedCaracter);
+            }
+
+            Console.WriteLine();
         }
     }
 }
+
